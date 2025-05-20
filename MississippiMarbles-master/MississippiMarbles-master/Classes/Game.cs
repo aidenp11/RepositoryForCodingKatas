0c2f1e3579ec002be2f44d515530b026a66f22cf
aidenp11 <apatrick@student.neumont.edu>
2025-05-20 13:47:00 -0600

    singleton for game class

--- MississippiMarbles-master/MississippiMarbles-master/Classes/Game.cs
+++ MississippiMarbles-master/MississippiMarbles-master/Classes/Game.cs
@@ -6,103 +6,122 @@ namespace MississippiMarbles.Classes
 {
 	internal class Game
 	{
+		private static Game _instance;
+		private static readonly object _lock = new object();
+
+		private List<Player> _players;
+		private int _turn;
 		public int winnerTurn;
 
-		public Game(List<Player> players, int turn)
+		private Game(List<Player> players)
 		{
-			Player player = players.ElementAt(turn);
-			player.diceNum = 6;
-			player.turn = true;
-			player.pointsToAdd = 0;
-			Roll roll;
+			_players = players;
+			_turn = 0;
+			StartGameLoop();
+		}
 
-			Console.WriteLine(player.getPlayerName + "'s turn\nScore: " + player.getPoints);
-
-			while (player.turn)
+		public static Game GetInstance(List<Player> players)
+		{
+			if (_instance == null)
 			{
-				if (player.diceNum == 0)
+				lock (_lock)
 				{
-					player.diceNum = 6;
-
-                    if (player.diceNum == 0)
-                    {
-                        player.setPoints(player.getPoints + player.pointsToAdd);
-                        Console.WriteLine("\n*** Turn Summary ***");
-                        Console.WriteLine("Player's total score for this turn: " + player.pointsToAdd);
-                        Console.WriteLine("Player's updated score: " + player.getPoints);
-                        Console.WriteLine("\n*** End of Turn ***\n");
-                        player.turn = true;
-                    }
-
-                    else
+					if (_instance == null)
 					{
-						player.setPoints(player.getPoints + player.pointsToAdd);
-						Console.WriteLine("Player score is now " + player.getPoints + '\n');
-						player.turn = false;
-						break;
+						_instance = new Game(players);
 					}
 				}
+			}
+			return _instance;
+		}
 
-                Console.WriteLine("\n*** Dice Status ***");
-                Console.WriteLine(player.diceNum + " dice left\nTotal score to be added for this turn: " + player.pointsToAdd);
-                Console.WriteLine("\n*** Choose Your Action ***");
-                Console.WriteLine("1) Roll");
-                Console.WriteLine("2) End Turn");
-				Console.Write("Choice: ");
-                string input = Console.ReadLine();
+		private void StartGameLoop()
+		{
+			bool gameEnded = false;
 
-                try
+			while (!gameEnded)
+			{
+				Player player = _players[_turn];
+				player.diceNum = 6;
+				player.turn = true;
+				player.pointsToAdd = 0;
+				Roll roll;
+
+				Console.WriteLine($"{player.getPlayerName}'s turn\nScore: {player.getPoints}");
+
+				while (player.turn)
 				{
-					int option = int.Parse(input);
+					if (player.diceNum == 0)
+					{
+						player.diceNum = 6;
 
-					if (option < 1 || option > 2)
-					{
-						Console.WriteLine("Input must be 1 or 2\n");
+						if (player.getPoints == 0 && player.pointsToAdd < 700)
+						{
+							Console.WriteLine("You need 700 or more points to start!\n");
+							break;
+						}
+						else
+						{
+							player.setPoints(player.getPoints + player.pointsToAdd);
+							Console.WriteLine("\n*** Turn Summary ***");
+							Console.WriteLine("Player's total score for this turn: " + player.pointsToAdd);
+							Console.WriteLine("Player's updated score: " + player.getPoints);
+							Console.WriteLine("\n*** End of Turn ***\n");
+							break;
+						}
 					}
-					else
+
+					Console.WriteLine("\n*** Dice Status ***");
+					Console.WriteLine($"{player.diceNum} dice left\nTotal score to be added for this turn: {player.pointsToAdd}");
+					Console.WriteLine("\n*** Choose Your Action ***");
+					Console.WriteLine("1) Roll");
+					Console.WriteLine("2) End Turn");
+					Console.Write("Choice: ");
+					string input = Console.ReadLine();
+
+					try
 					{
+						int option = int.Parse(input);
+
 						switch (option)
 						{
 							case 1:
 								roll = new Roll(player);
-								roll.TryOpen(player.diceNum);  // Handle the roll of the dice
+								roll.TryOpen(player.diceNum);
 								break;
 
-                            case 2:
-                                if (player.getPoints == 0 && player.pointsToAdd < 700)
-                                {
-                                    Console.WriteLine("You need 700 or more points to start!\n");
-                                    player.turn = false;
-                                }
-                                else
-                                {
-                                    player.setPoints(player.getPoints + player.pointsToAdd);
-                                    Console.WriteLine("Player score is now " + player.getPoints + '\n');
-                                    player.turn = false;
-                                }
-                                break;
-                        }
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Input must be 1 or 2\n");
-                }
-            }
+							case 2:
+								if (player.getPoints == 0 && player.pointsToAdd < 700)
+								{
+									Console.WriteLine("You need 700 or more points to start!\n");
+									player.turn = false;
+								}
+								else
+								{
+									player.setPoints(player.getPoints + player.pointsToAdd);
+									Console.WriteLine("Player score is now " + player.getPoints + '\n');
+									player.turn = false;
+								}
+								break;
 
-			if (player.getPoints >= 11000)
-			{
-				winnerTurn = turn;
-				return;
-			}
-			else if (players.Count == turn + 1)
-			{
-				turn = 0;
-				new Game(players, turn);
-			}
-			else
-			{
-				new Game(players, turn + 1);
+							default:
+								Console.WriteLine("Input must be 1 or 2\n");
+								break;
+						}
+					}
+					catch (FormatException)
+					{
+						Console.WriteLine("Input must be 1 or 2\n");
+					}
+				}
+
+				if (player.getPoints >= 11000)
+				{
+					winnerTurn = _turn;
+					gameEnded = true;
+				}
+
+				_turn = (_turn + 1) % _players.Count;
 			}
 		}
 	}
